8b63ac7d72c21ce99bef31e485ec12c5b8b663db
yappisachin007 <yappi.sachin@gmail.com>
2019-04-14 19:06:49 +0530

    add

---
+++ My_First_Project/src/com/java/Test.java
@@ -0,0 +1,20 @@
+package com.java;
+
+
+public class Test {
+
+
+	public Test() {
+		System.out.println("om");
+	}
+
+	public static void main(String[] args) {
+		Test obj = new Test();
+		obj.disp();
+	}
+
+	private void disp() {
+		System.out.println("oMomoMOmOMOMOM");
+	}
+
+}
